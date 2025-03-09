97ae01d9e040918d59c9e4c26ac24e36c9d014d8
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2025-03-09 16:42:17 +0100

    add nix flake and uv python environment

---
+++ flake.nix
@@ -0,0 +1,129 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      uv2nix,
+      pyproject-nix,
+      flake-utils,
+      pyproject-build-systems,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (nixpkgs) lib;
+
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+        snake-overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
+          # sourcePreference = "sdist";
+        };
+
+        pyprojectOverrides = _final: _prev: { };
+
+        python = pkgs.python312;
+
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                snake-overlay
+                pyprojectOverrides
+              ]
+            );
+
+      in
+      {
+        packages.default = pythonSet.mkVirtualEnv "snake-env" workspace.deps.default;
+
+        devShells.default =
+          let
+            editableOverlay = workspace.mkEditablePyprojectOverlay {
+              root = "$REPO_ROOT";
+            };
+
+            editablePythonSet = pythonSet.overrideScope (
+              lib.composeManyExtensions [
+                editableOverlay
+
+                (final: prev: {
+                  snake = prev.snake.overrideAttrs (old: {
+                    src = lib.fileset.toSource {
+                      root = old.src;
+                      fileset = lib.fileset.unions [
+                        (old.src + "/pyproject.toml")
+                        (old.src + "/README.md")
+                        (old.src + "/src/snake/")
+                      ];
+                    };
+
+                    nativeBuildInputs =
+                      old.nativeBuildInputs
+                      ++ final.resolveBuildSystem {
+                        editables = [ ];
+                      };
+                  });
+
+                })
+              ]
+            );
+            virtualenv = editablePythonSet.mkVirtualEnv "snake-dev-env" workspace.deps.all;
+
+          in
+          pkgs.mkShell {
+            packages = [
+              virtualenv
+              pkgs.uv
+              pkgs.pre-commit
+              pkgs.texliveMinimal
+              pkgs.ruff
+            ];
+
+            nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+            shellHook = ''
+              patch=$(autoPatchelf ~/.cache/pre-commit/)
+
+              unset PYTHONPATH
+              export REPO_ROOT=$(git rev-parse --show-toplevel)
+            '';
+
+            env = {
+              UV_NO_SYNC = "1";
+              UV_PYTHON = "${virtualenv}/bin/python";
+              UV_PYTHON_DOWNLOADS = "never";
+            };
+          };
+      }
+    );
+}
